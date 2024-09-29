60b10fe45deeed40376fdb2e34e11dfa5ba85b59
adnanZahid675 <adnanzahidalvi675@gmail.com>
2024-09-29 19:30:42 +0500

    checking logs

--- controllers/callStreaming.js
+++ controllers/callStreaming.js
@@ -11,14 +11,31 @@ const getCallStreaming = (req, res) => {
   const callSocket = new WebSocket.Server({ noServer: true });
   callSocketServers[conf_sid] = callSocket;
   callSocketServers[conf_sid].on("connection", (ws) => {
+    // ws.on("message", (message) => {
+    //   console.log(`Received message: ${message}`);
+    //   ws.send(`Server response: ${message}`);
+    // });
+    // ws.on("close", () => {
+    //   console.log("Client disconnected");
+    // });
+
+    console.log("Client connected to WebSocket");
+
     ws.on("message", (message) => {
       console.log(`Received message: ${message}`);
       ws.send(`Server response: ${message}`);
     });
+
     ws.on("close", () => {
       console.log("Client disconnected");
     });
+
+    ws.on("error", (error) => {
+      console.error(`WebSocket error: ${error}`);
+    });
   });
+  console.log("\n\nreq.headers.host", req.headers.host);
+
   const wsUrl = `ws://${req.headers.host}/callStreaming?call_sid=${conf_sid}`;
   res.json({ message: "WebSocket server for call has created", url: wsUrl });
 };

3b8826d5f6a687d5f2bca49646d8a3b66d9c04db
adnanZahid675 <adnanzahidalvi675@gmail.com>
2024-10-02 20:48:36 +0500

    sockets

--- controllers/callStreaming.js
+++ controllers/callStreaming.js
@@ -29,8 +29,7 @@ const getCallStreaming = (req, res) => {
   });
 
   // Check if the incoming request is secure (HTTPS) to decide between ws or wss
-  const protocol = req.secure ? "wss" : "ws"; // req.secure is true if HTTPS
-  const wsUrl = `${protocol}://${req.headers.host}/callStreaming?call_sid=${conf_sid}`;
+  const wsUrl = `wss://${req.headers.host}/callStreaming?call_sid=${conf_sid}`;
 
   res.json({
     message: "WebSocket server for call has been created",

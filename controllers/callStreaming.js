7515a28c5963fad23912cd8eadfe1ae67eb00e76
adnanZahid675 <adnanzahidalvi675@gmail.com>
2024-10-16 16:43:24 +0500

    adsf

--- controllers/callStreaming.js
+++ controllers/callStreaming.js
@@ -251,8 +251,8 @@ const calling_to_owner = async (req, res) => {
     <?xml version="1.0" encoding="UTF-8"?>
      <Response>
         <Say>Connecting you to Person B now.</Say>
-        <Dial callerId="${from}" action="https://callstream-6b64fe9b1f4d.herokuapp.com/api/bridge_end" hangupOnStar="false" endOnBridge="false">
-          <Number>${to}</Number>
+        <Dial callerId="${from}" DialCallStatus="completed" action="https://callstream-6b64fe9b1f4d.herokuapp.com/api/bridge_end">
+          <Number statusCallbackEvent="completed" statusCallback="https://callstream-6b64fe9b1f4d.herokuapp.com/api/bridge_end">${to}</Number>
         </Dial>
       </Response>`;
 

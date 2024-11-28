654389def0790dd91725902dcda8c887dcccabe5
adnanZahid675 <adnanzahidalvi675@gmail.com>
2024-11-28 15:03:33 +0500

    db config

--- config/db.config.js
+++ config/db.config.js
@@ -5,13 +5,23 @@ dotenv.config();
 
 async function connectMySQL() {
   try {
+
+    console.log("all configs",process.env.DB_HOST,process.env.DB_USER );
+    
     const connection = await mysql.createConnection({
-      host: process.env.DB_HOST || "127.0.0.1", // Default to local IP
-      user: process.env.DB_USER || "root",      // Default user
-      password: process.env.DB_PASS || "",      // Default password
-      database: process.env.DB_NAME || "invisible", // Default database
-      port: process.env.DB_PORT || 3306         // Default MySQL port
+      host: "127.0.0.1", // Default to local IP
+      user: "root", // Default user
+      password: "5drYHiltWQt7bQT", // Default password
+      database: "invisible", // Default database
+      port: 3306, // Default MySQL port
     });
+    // const connection = await mysql.createConnection({
+    //   host: process.env.DB_HOST || "127.0.0.1", // Default to local IP
+    //   user: process.env.DB_USER || "root",      // Default user
+    //   password: process.env.DB_PASS || "",      // Default password
+    //   database: process.env.DB_NAME || "invisible", // Default database
+    //   port: process.env.DB_PORT || 3306         // Default MySQL port
+    // });
 
     console.log("Database connected successfully!");
     return connection;
